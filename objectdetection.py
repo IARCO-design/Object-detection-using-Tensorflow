169efdb06bef61a44465e3b39ffaabee27a5e459
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2019-10-22 16:49:06 +0200

    improved load time
    improved load time by having model file downloaded localy

--- objectdetection.py
+++ objectdetection.py
@@ -40,8 +40,6 @@ from object_detection.utils import visualization_utils as vis_util
 
 # What model to download.
 MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
@@ -55,9 +53,7 @@ NUM_CLASSES = 90
 # ## Download Model
 
 # In[5]:
-
-opener = urllib.request.URLopener()
-opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+MODEL_FILE='ssd_mobilenet_v1_coco_11_06_2017.tar.gz'
 tar_file = tarfile.open(MODEL_FILE)
 for file in tar_file.getmembers():
   file_name = os.path.basename(file.name)
